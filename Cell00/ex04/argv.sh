4bc256b4c7ba0e2e2a926ee33e7efc2fc32b92c3
peerapat138 <66070138@kmitl.ac.th>
2026-09-17 16:27:15 +0700

    update

---
+++ Cell00/ex04/argv.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for i in {1..3}; do
+        if [ $i -le $# ]; then
+            echo "${!i}"
+        else
+            break
+        fi
+    done
+fi
